bc1fac9df7c9fb2e6457e789b6bfd36185ef3187
Huijuan Wang <LemonNoel@gmail.com>
2017-05-31 12:28:40 +0800

    Chat Activity Finished

---
+++ app/src/main/java/com/aands/wefamily/Chat/ChatAdapter.java
@@ -0,0 +1,10 @@
+package com.aands.wefamily.Chat;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by LemonNoel on 2017/5/31.
+ */
+
+public class ChatAdapter extends RecyclerView {
+}
