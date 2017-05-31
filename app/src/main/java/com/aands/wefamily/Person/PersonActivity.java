b2d286a3679efbf7986c93f63c2fd19e27c10bc9
conniemessi <conniemessi@163.com>
2017-05-31 15:38:47 +0800

    Person2

--- app/src/main/java/com/aands/wefamily/Person/PersonActivity.java
+++ app/src/main/java/com/aands/wefamily/Person/PersonActivity.java
@@ -1,24 +1,42 @@
 package com.aands.wefamily.Person;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
 import com.aands.wefamily.Contact.ContactActivity;
+import com.aands.wefamily.Family.Family;
 import com.aands.wefamily.R;
+
+import org.litepal.crud.DataSupport;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.aands.wefamily.Constants.EDIT_CONTACT_PERSON;
 
 /**
  * Created by renwendi on 17/5/31.
  */
 
-public class PersonActivity extends Activity {
+public class PersonActivity extends AppCompatActivity {
+    private Context getContext() {
+        return this;
+    }
+    private Button test;
+    private Family familyItem = new Family();
+
+    /*public void btnclick(View v){
+        Intent intent=new Intent(PersonActivity.this,ContactActivity.class);
+        startActivity(intent);
+    }*/
+
     private final static String[] data = {"大伯","小舅"};
 
     //创建数据源.
@@ -31,6 +49,20 @@ public class PersonActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_person);
+        Intent intent = getIntent();
+        final String name = intent.getStringExtra("name");
+        List<Family> tmpList = DataSupport.where("name = ?", name).find(Family.class);
+        if (tmpList.size() > 0) {
+            familyItem = tmpList.get(0);
+            test = (Button) findViewById(R.id.button_person);
+            test.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ContactActivity.actionStart(getContext(), EDIT_CONTACT_PERSON, name);
+                }
+            });
+        }
+
 
         ListView listview = (ListView)findViewById(R.id.listview);
 
@@ -70,3 +102,4 @@ public class PersonActivity extends Activity {
         });*/
     }
 }
+
